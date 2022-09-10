45978ab596edf34076950f2d86f2e001340c0e9f
mkauaa <kauaa.matos@gmail.com>
2022-09-10 13:05:12 -0300

    Update ex01.py

--- ex01.py
+++ ex01.py
@@ -1,16 +1,30 @@
 # Questão 01. Fazer um programa para ler uma lista de 5 elementos, e, em seguida, mostrar
 # a posição onde se encontram o maior e o menor valor e seus respectivos valores.
 
-lista = [4, 3, 5, 2, 1]
+from random import randint
 
-maior = max(lista)
-menor = min(lista)
+lista = []
 
-for i, v in enumerate(lista):
-    if v == maior:
-        indiceMaior = i
-    if v == menor:
-        indiceMenor = i
+cont = 0
 
-print(f'O maior valor informado foi {maior} e se encontra na posição {indiceMaior + 1}')
-print(f'O menor valor informado foi {menor} e se encontra na posição {indiceMenor + 1}')
+while True:
+    num = randint(1,10)
+
+    if num not in lista:
+        lista.append(num)
+        cont += 1
+
+    if cont == 5:
+        break
+
+numMaior = max(lista)
+numMenor = min(lista)
+
+indiceMaior = lista.index(numMaior)
+indiceMenor = lista.index(numMenor)
+
+print(f'Lista: {lista}')
+
+
+print(f'O maior valor gerado foi {numMaior} e se encontra na posição {indiceMaior + 1}.')
+print(f'O menor valor gerado foi {numMenor} e se encontra na posição {indiceMenor + 1}.')
