c6283d13d93269121d8a57d34272784b14782031
Kauã Andrade <kauaa.matos@gmail.com>
2022-10-07 20:14:00 -0300

    def compara & fixed remove

--- pilha_classe.py
+++ pilha_classe.py
@@ -1,3 +1,6 @@
+from operator import truediv
+
+
 class Nodo:
     def __init__(self, dado = 0, nodo_anterior = None):
         self.dado = dado
@@ -31,8 +34,24 @@ class Pilha:
             print('É impossível remover valores de uma pilha vazia.')
         
         else:
-            removido = self.topo
+            removido = self.topo.dado
             self.topo = self.topo.anterior
             self.tamanho -= 1
             return removido
-            
+          
+    def compara(self, pilha2):
+      if self.tamanho != pilha2.tamanho:
+        return False
+      
+      else:
+        no_atual1 = self.topo
+        no_atual2 = pilha2.topo
+        
+        while (no_atual1) != None:
+          if (no_atual1.dado != no_atual2.dado):
+            return False
+          
+          no_atual1 = no_atual1.anterior
+          no_atual2 = no_atual2.anterior
+          
+        return True
