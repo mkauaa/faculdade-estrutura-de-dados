e67cc975821c7a762b5d04f9fa82a8c4eb246285
Kauã Andrade <kauaa.matos@gmail.com>
2022-10-07 20:15:32 -0300

    compara

--- pilha_exec.py
+++ pilha_exec.py
@@ -10,8 +10,21 @@ for v in range(5):
     print(f'Insere o valor {v} no topo da pilha: {P}')
     print(P.tamanho)
 
+#for v in range(5):
+#    P.remove()
+#    print(f'Remove o valor do topo da pilha:   {P}')
+#    print(P.tamanho)
+
 for v in range(5):
-    P.remove()
-    print(f'Remove o valor do topo da pilha:   {P}')
-    print(P.tamanho)
+    L.insere(v)
+    print(f'Insere o valor {v} no topo da pilha: {L}')
+    print(L.tamanho)
+    
+print(P.compara(L))
+
+if P.compara(L):
+  print('As pilhas são iguais.')
+
+elif not P.compara(L):
+  print('As pilhas são diferentes.')
     
