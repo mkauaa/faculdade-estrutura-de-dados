60a95ddec7f1be73babcc4c1cb0745a88162615e
Kauã Andrade <kauaa.matos@gmail.com>
2022-10-07 21:26:46 -0300

    Create fila_classe.py

---
+++ fila_classe.py
@@ -0,0 +1,43 @@
+class NodoFila: 
+  def __init__(self, dado = 0, proximo_nodo = None):
+    self.dado = dado
+    self.proximo = proximo_nodo
+    
+  def __repr__(self):
+      return f'{self.dado} -> {self.proximo}'
+    
+class Fila:
+  def __init__(self):
+      self.cabeca = None
+      self.cauda = None
+      
+  def __repr__(self):
+      return "[" + str(self.cabeca) + "]"
+    
+  def vazia(self):
+    if self.cabeca == None:
+      return True
+    
+    else:
+      return False
+    
+  def insere(self, novo_dado):
+    novo_nodo = NodoFila(novo_dado)
+    
+    if self.vazia():
+      self.cabeca = novo_nodo
+      self.cauda = novo_nodo
+      
+    else:
+      self.cauda.proximo = novo_nodo
+      self.cauda = novo_nodo
+      
+  def remove(self):
+    removido = self.cabeca.dado
+    
+    self.cabeca = self.cabeca.proximo
+    if self.cabeca == None:
+      self.cauda = None
+      
+    return removido
+  
