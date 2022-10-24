253ba5b7f70af309b872877aaddb329b5c00d437
mkauaa <kauaa.matos@gmail.com>
2022-10-24 18:55:30 -0300

    lista

--- Classes/lista_classes.py
+++ Classes/lista_classes.py
@@ -3,17 +3,17 @@ class Nodo:
     self.dado = dado
     self.proximo = proximo_nodo
     
-    def __repr__(self):
-      return f'{self.dado} -> {self.proximo}'
+  def __repr__(self):
+    return f'{self.dado} -> {self.proximo}'
 
 class Lista:
   def __init__(self):
     self.cabeca = None
-    self.cauda - None
+    self.cauda = None
     self.tamanho = 0
     
   def __repr__(self):
-    return f'{[str(self.cabeca)]}'
+    return f'[{str(self.cabeca)}]'
   
   def vazia(self):
     if self.cabeca == None:
@@ -27,6 +27,7 @@ class Lista:
     if self.vazia():
       self.cabeca = novo_nodo
       self.cauda = novo_nodo
+      self.tamanho += 1
       
     else:
       novo_nodo.proximo = self.cabeca
@@ -39,9 +40,10 @@ class Lista:
     if self.vazia():
       self.cabeca = novo_nodo
       self.cauda = novo_nodo
+      self.tamanho += 1
       
     else:
-      self.cauda.proximo = self.cauda
+      self.cauda.proximo = novo_nodo
       self.cauda = novo_nodo
       self.tamanho += 1
   
@@ -75,27 +77,55 @@ class Lista:
         if atual == self.cauda:
           self.cauda = anterior
           
-          anterior.proximo = atual.proximo
-          self.tamanho -= 1
+        anterior.proximo = atual.proximo
+        self.tamanho -= 1
           
-          return removido
+        return removido
       
     else:
       return 'Posição inválida.'
     
-    def busca(self, dado):
-      if self.vazia():
-        return 'A lista está vazia.'
+  def busca(self, dado):
+    if self.vazia():
+      return 'A lista está vazia.'
+    
+    else:
+      atual = self.cabeca
+      posicao = 0
       
+      while atual.dado != dado:
+        atual = atual.proximo
+        posicao += 1
+        
+        if posicao == self.tamanho:
+          return 'Dado não encontrado na lista.'
+      
+      return posicao
+
+  def inserePosicao(self, novo_dado, posicao):
+    if 0 <= posicao <= self.tamanho:
+      if posicao == 0:
+        self.insereInicio(novo_dado)
+        self.tamanho += 1
+      
+      elif posicao == (self.tamanho):
+        self.insereFim(novo_dado)
+        self.tamanho += 1
+
       else:
         atual = self.cabeca
-        posicao = 0
+        i = 0
         
-        while atual.dado != dado:
+        while i != posicao:
+          anterior = atual
           atual = atual.proximo
-          posicao += 1
-          
-          if posicao == self.tamanho:
-            return 'Dado não encontrado na lista.'
+          i += 1
         
-        return posicao
+        # adaptar a partir daq
+        removido = atual.dado
+        
+        if atual == self.cauda:
+          self.cauda = anterior
+          
+        anterior.proximo = atual.proximo
+        self.tamanho -= 1
---
+++ Listas/lista_teste.py
@@ -0,0 +1,28 @@
+import sys
+sys.path.append(".")
+
+from Classes.lista_classes import Lista
+
+lista = Lista()
+
+if lista.vazia():
+    print('\nA lista está vazia. Adicionando elementos...')
+
+lista.insereInicio(1)
+lista.insereInicio(2)
+lista.insereFim(4)
+
+print(lista)
+
+print(f'A lista tem {lista.tamanho} elementos.')
+print(f'Removendo o elemento "{lista.remove(1)}"...')
+
+print(lista)
+
+print(f'A lista tem {lista.tamanho} elementos.')
+
+print(f'Procurando o elemento "4"... \nAchei! Na posição {lista.busca(4)}')
+
+lista.inserePosicao(99, 2)
+
+print(lista)
